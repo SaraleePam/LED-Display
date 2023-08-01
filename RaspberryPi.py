bc2450cddf014e7431520655cba8be1edb5c3b0d
SaraleePam <109116723+SaraleePam@users.noreply.github.com>
2023-08-01 13:55:27 -0400

    Set up API

--- RaspberryPi.py
+++ RaspberryPi.py
@@ -1,9 +1,27 @@
-import serial
 import time
-import datetime
+import serial
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 
-microsteps = 16
-day = 0
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# The ID and range of the spreadsheet
+SPREADSHEET_ID = '1VOnFzvLX1b26OtMFG4rKbH1HoCr9RUDB_HlAsYFuMKE'
+RANGE_NAME = 'Sheet1!A1:B'
+
+
+# Get credentials from the token file
+creds = Credentials.from_authorized_user_file('/Users/saralees/Documents/GitHub/LED-Display/GoogleAPI/token.json', SCOPES)
+service = build('sheets', 'v4', credentials=creds)
+
+# Call the Sheets API
+sheet = service.spreadsheets()
+result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+values = result.get('values', [])
+
+######
+
 
 ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
 print("Initializing connection....")
@@ -11,26 +29,24 @@ time.sleep(3)
 ser.reset_input_buffer()
 print("HELLO!")
 print("connected to: " + ser.portstr)
-print("moving")
 
 
-def get_second_hand_angle():
-    now = datetime.datetime.now()
-    minute = now.minute
-    second = now.second
-    hour = now.hour
-    currentsec = (hour * 3600) + (minute * 60) + second
-    return (currentsec / 86400.0) * 360.0
+# def get_text():
+#     now = datetime.datetime.now()
+#     minute = now.minute
+#     second = now.second
+#     hour = now.hour
+#     currentsec = (hour * 3600) + (minute * 60) + second
+#     return (currentsec / 86400.0) * 360.0
 
 
 
 while True:
 
     time.sleep(5)
-    stepangle = int(get_second_hand_angle()  /360.0 * 800.0 * microsteps)
-    print(stepangle)
-    ser.write(('UTC_MOVE ' + str(stepangle) + '\n').encode())
+    print(values)
+    ser.write(('TEXT ' + str(values) + '\n').encode())
     
 
            
-#ser.close()
+# #ser.close()
